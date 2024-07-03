e3cb01d00494f4037f713c2cebb50c5f86fbf624
Victor Ordonez <hda.victor.ordonez1993@gmail.com>
2024-07-03 09:32:54 +0530

    Initial Commit

---
+++ src/main/java/com/houstondirectauto/refurb/RefurbApplication.java
@@ -0,0 +1,13 @@
+package com.houstondirectauto.refurb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefurbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefurbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/houstondirectauto/refurb/RefurbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houstondirectauto.refurb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefurbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
