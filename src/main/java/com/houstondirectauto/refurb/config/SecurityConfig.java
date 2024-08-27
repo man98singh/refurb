b8416539334e8f498bb7a0e0162e991a0afbb622
Victor Ordonez <hda.victor.ordonez1993@gmail.com>
2024-08-27 11:12:37 +0530

    allow all method in the SecurityConfig

--- src/main/java/com/houstondirectauto/refurb/config/SecurityConfig.java
+++ src/main/java/com/houstondirectauto/refurb/config/SecurityConfig.java
@@ -94,7 +94,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:3000","*"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
+        configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
