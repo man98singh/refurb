de0c7b668984a84f0575ffb896c63a5fb1c6153d
manish-astrid <manish.singh@astridinfotech.com>
2024-10-03 16:16:59 +0530

    Fixed resonse with object including /verify, removed constants

--- src/main/java/com/houstondirectauto/refurb/model/TwoFactorAuthResponse.java
+++ src/main/java/com/houstondirectauto/refurb/model/TwoFactorAuthResponse.java
@@ -1,9 +1,14 @@
 package com.houstondirectauto.refurb.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
 public class TwoFactorAuthResponse {
     private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String generatedCode;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean success;
+
 }
